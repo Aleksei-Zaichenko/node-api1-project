b4cf7f99da4c79edbbf3837fe19a3f9ad1be33fb
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-08 01:15:10 -0400

    completed delete and put endpoints

--- index.js
+++ index.js
@@ -41,70 +41,62 @@ server.get('/api/users/:id', (req, res) => {
     });
   }
 });
-  
-  server.post('/api/users', (req, res) => {
-    if(req.body.name && req.body.bio){
-      const newUser = {id: shortid.generate(), ...req.body }
-      if(newUser){
-        users.push(newUser);
-        res.status(201).json(newUser);
-      } else {
-        res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
-      }
-    } else {
-        res.status(400).json({
-            errorMessage:"Please provide name and bio for the user."
-        });
-    }
-  });
- 
-  server.delete('api/users/:id', (req, res) => {
-    const foundUser = users.find(user=> user.id == req.params.id)
-    if (foundUser) {
-      const newUsersList = users.filter(user => user.id != req.params.id);
-      users = newUsersList;
 
-      if(newUsersList.length === users.length){
-        res.status(200).json({message:"The user was deleted successfully"})
-      } else {
-        res.status(500).json({ errorMessage: "The user could not be removed" });
-      }
-
+server.post('/api/users', (req, res) => {
+  if(req.body.name && req.body.bio){
+    const newUser = {id: shortid.generate(), ...req.body }
+    if(newUser){
+      users.push(newUser);
+      res.status(201).json(newUser);
     } else {
-      res.status(404).json({ message: "The user with the specified ID does not exist." });
+      res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
     }
-  });
+  } else {
+      res.status(400).json({
+          errorMessage:"Please provide name and bio for the user."
+      });
+  }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+
+  const foundUser = users.find(user=> user.id == (req.params.id))
+
+  if (foundUser) {
+    const newUsersList = users.filter(user => user.id != req.params.id);
+    users = newUsersList;
+
+    if(newUsersList.length === users.length){
+      res.status(200).json({message:"The user was deleted successfully"})
+    } else {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    }
+  } else {
+    res.status(404).json({ message: "The user with the specified ID does not exist." });
+  }
+});
     
-  //   router.put('/:id', (req, res) => {
-      
-  //     const changes = req.body;
-  
-  //     if(!changes.text && !changes.contents){
-  //       res.status(400).json({errorMessage: "Please provide title and contents for the post."})
-  //     } else {
-  //       PostsDataBase.update(req.params.id, changes)
-  //       .then(hub => {
-  //         if (hub) {  
-  //           PostsDataBase.findById(req.params.id)
-  //           .then(hub =>{
-  //             res.status(200).json(hub);
-  //           })
-  //           .catch(err =>{
-  //             res.status(500).json({errorMessage: "error reading the updated  "})
-  //           })
-  //         } else {
-  //           res.status(404).json({ message: "The post with the specified ID does not exist." });
-  //         }
-  //       })
-  //       .catch(error => {
-  //         // log error to database
-  //         console.log(error);
-  //         res.status(500).json({
-  //           error: "The post information could not be modified."
-  //         });
-  //       });
-  //     }
-      
-  //   });
+server.put('/api/users/:id', (req, res) => {
+  const idOfAUser = req.params.id
+  const changes = req.body;
 
-server.listen(5000, () => console.log('API running on port 5000'));
+  if(!changes.name || !changes.bio){
+    res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+  } else {
+    const foundUser = users.find(user => user.id == req.params.id)
+
+      if (foundUser) {
+          const indexOfUserToChange = users.indexOf(foundUser);
+          users[indexOfUserToChange] = {id:idOfAUser,...req.body};
+          if(foundUser !== users[indexOfUserToChange]){
+            res.status(200).json(users[indexOfUserToChange]);
+          } else {
+            res.status(500).json({errorMessage: "The user information could not be modified."});
+          }
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+    }
+});
+
+server.listen(3000, () => console.log('API running on port 5000'));
